c36dde62dd9602905a012407ca19d93b65b5a8f0
Rinat Katipov <rinat777hatipov@gmail.com>
2018-11-05 20:52:07 +0300

    Fix contract

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React, {useState, Component} from "react"
 import logo from "./logo.svg"
 import contract from "./contract"
+import web3 from "./web3"
+
 import "./App.css"
 
 interface AppInterface {
   message: string
+  value: string
 }
 class App extends Component<{}, AppInterface> {
   state: AppInterface = {
     message: "",
+    value: "",
   }
   async componentDidMount() {
     const message = await contract.methods.message().call()
@@ -17,20 +21,25 @@ class App extends Component<{}, AppInterface> {
       message,
     })
   }
+  // @ts-ignore
+  onSubmit = async () => {
+    const accounts = await web3.eth.getAccounts
+    await contract.methods.setMessage(this.state.value).send({
+      // @ts-ignore
+      from: accounts[0],
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>{this.state.message}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <input
+            value={this.state.value}
+            onChange={event => this.setState({value: event.target.value})}
+          />
+          <button onClick={}>Change message</button>
         </header>
       </div>
     )
