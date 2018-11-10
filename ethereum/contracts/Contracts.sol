08263cc35f19e1e2a9250781977113d319e1fa71
Rinat Katipov <rinat777hatipov@gmail.com>
2018-11-10 21:23:01 +0300

    Add contracts

--- ethereum/contracts/Contracts.sol
+++ ethereum/contracts/Contracts.sol
@@ -1,28 +1,70 @@
 pragma solidity ^0.4.24;
 
 contract Factory {
-  address[] public deployedContracts;
+  address[] public deployedStations;
+  address[] public stationManagers;
 
-  function createContract(string message) public {
-    address newContract = new Contract(message, msg.sender);
-    deployedContracts.push(newContract);
+  function createStation() public {
+    address newStation = new Station(msg.sender);
+    deployedStations.push(newStation);
+    stationManagers.push(msg.sender);
   }
 
-  function getDeployedContracts() public view returns (address[]) {
-    return deployedContracts;
+  function getStationManagers() public view returns (address[]) {
+    return stationManagers;
+  }
+
+  function getDeployedStations() public view returns (address[]) {
+    return deployedStations;
   }
 }
 
-contract Contract {
-  string public currentMessage;
+contract Station {
+  bool public approved;
   address public manager;
+  mapping(address => bool) public operators;
 
-  constructor(string message, address creator) public {
-    currentMessage = message;
+  constructor(address creator) public {
+    approved = false;
     manager = creator;
+    operators[0x3b3DE2C94e6d0eCA853a0e45Bbf2E10636Cc02aA] = true; 
   }
 
-  function changeMessage(string mess) public {
-    currentMessage = mess;
+  modifier restrictedManager() {
+    require(msg.sender == manager, "Fuck u");
+    _;
+  }
+
+  modifier restrictedOperator() {
+    require(operators[msg.sender], "Fuck u");
+    _;
+  }
+
+  function isApproved() public view returns (bool) {
+    return approved;
+  }
+
+  function approveStation() public restrictedOperator {
+    approved = true;
+  }
+}
+
+contract Tokens {
+  mapping(address => uint) public tokenHolders;
+
+  function addTokens(address holderAddress, uint quantity) public {
+    if (tokenHolders[holderAddress] != 0) {
+      tokenHolders[holderAddress] += quantity;
+    } else {
+      tokenHolders[holderAddress] = quantity;
+    }
+  }
+  function getTokens(address holderAddress) public view returns (uint) {
+    return tokenHolders[holderAddress];
+  }
+
+  function burnTokens(address holderAddress, uint quantity) public {
+    require(tokenHolders[holderAddress] != 0 && tokenHolders[holderAddress] <= quantity, "Suck");
+    tokenHolders[holderAddress] -= quantity;
   }
 }
