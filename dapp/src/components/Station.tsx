59d4b10c2159c5c5a2a174ad5c9c24b047fc8946
Goldobin Anton <722451@it-atlantic.com>
2018-11-10 16:18:04 +0300

    first page

--- dapp/src/components/Station.tsx
+++ dapp/src/components/Station.tsx
@@ -36,8 +36,8 @@ class Station extends Component<{}, StationInterface> {
     return (
           <div className="row">
             <div className="col-4">
-              <div className="pr-4">
-                <div className="tokens-title">tokens</div>
+              <div>
+                <div className="mt-3 tokens-title">tokens</div>
                 <div className="tokens-int">43.5</div>
                 <div className="ether-title">etherium wallet</div>
                 <div className="ether-int">{this.state.balance}</div>
@@ -58,9 +58,10 @@ class Station extends Component<{}, StationInterface> {
                   </div>
                   <a className="d-flex justify-content-center">or buy tokens</a>
                   <div className="line w-100 " />
-                    <div className='row'>
+                    <div className='row mt-5'>
                         <div className='col-6'>
-                          <p className="card-text mt-5">
+                            <p className='card-title'>Full pull stats</p>
+                          <p className="card-text mt-3">
                             Some quick example text to build on the card title and make
                             up the bulk of the card's content.
                           </p>
@@ -70,10 +71,18 @@ class Station extends Component<{}, StationInterface> {
               </div>
             </div>
               <div className='col-4'>
+                  <div className='stats-green-info d-flex flex-row align-items-center'>
+                      <span className='green-cube'></span>
+                      <div>
+                          <p className='stats-green-title'>20% Green Energy</p>
+                          <p className='stats-green-dest'>1243 tokens in pull</p>
+                      </div>
+                  </div>
                   <div className='stats'>
                       <DonutChart data={dataPie} spacing={1} />
-                      <div className='d-flex stats-title align-items-center justify-content-center'>
-                        Test
+                      <div className='d-flex stats-title align-items-center flex-column justify-content-center'>
+                        20 342
+                          <span className='stats-sub'>tokens inside pull</span>
                       </div>
                   </div>
               </div>
